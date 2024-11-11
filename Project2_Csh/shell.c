bf746f6df69bffd54114a7236695f7509bce88a9
rzeta-10 <125035257+rzeta-10@users.noreply.github.com>
2024-11-11 19:49:27 +0530

    Update shell.c

--- Project2_Csh/shell.c
+++ Project2_Csh/shell.c
@@ -27,30 +27,6 @@
 #define DT_DIR 4  // Value for directories
 #endif
 
-// colour guide - https://github.com/shiena/ansicolor/blob/master/README.md
-/*
-* Usage - make clean ; clear ; make ; ./Cshell
-*/
-/* implement shell functions
-*/
-/* Instructions to support :
-*  cd <dir>                                 - Done cd
-*  pwd                                      - Done pwd
-*  mkdir <dir>                              - Done mkdir
-*  rmdir <dir>                              - Done rmdir
-*  ls (support ls -l)                       - Done ls & -l
-*  cp <file1> <file2>                       - Done cp
-*  exit                                     - Done exit
-*  execute any other command like ./a.out   - Done exec
-* support background execution - &          - Done &
-* redirect input output >, <                - Done
-*  a.out | b.out - must support a| b| c     - Done
-* Additional - clear
-* Additional - screenfetch
-* Additional - up down keys, history- ?
-* Additional - tab keys autocompletiton
-*/
-
 struct _instr
 {
     char * argval[INPBUF];
